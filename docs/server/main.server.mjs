a781b897e0112fb140c1334cc57761757e25ec7d
Juan Tombal <juantombal@live.nl>
2024-07-26 16:12:46 +0200

    t

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-UVZ7LPVE.mjs";import"./chunk-FQWUU27Z.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { TeamComponent } from './components/team/team.component';
+import { EnrollComponent } from './components/enroll/enroll.component';
+import { AppointmentComponent } from './components/appointment/appointment.component';
+import { PricesComponent } from './components/prices/prices.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'inschrijven', component: EnrollComponent },
+  { path: 'afspraak', component: AppointmentComponent },
+  { path: 'team', component: TeamComponent },
+  { path: 'behandelingen-tarieven', component: PricesComponent },
+  { path: 'contact', component: ContactComponent },
+];
